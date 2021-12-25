adcdcf28c523c286937edc5b100ff41541a48b14
ThomasFercher <46001083+ThomasFercher@users.noreply.github.com>
2021-12-25 20:01:12 +0100

    improve login page

--- lib/pages/utility/login_page.dart
+++ lib/pages/utility/login_page.dart
@@ -41,151 +41,169 @@ class LoginPage extends StatelessWidget {
                 alignment: Alignment.centerRight,
                 child: Padding(
                   padding: const EdgeInsets.only(right: 120),
-                  child: Container(
-                    width: 320,
-                    height: 420,
-                    child: Card(
-                      color: theme.colors.cardBackgroundColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: theme.sizing.borderRadius[0],
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(
-                          24,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 64,
+                        child: LegendText(
+                          text: "Adleft",
+                          textStyle: theme.typography.h6,
                         ),
-                        child: Column(
-                          children: [
-                            LegendText(
-                              text: "Login",
-                              textStyle: theme.typography.h5,
+                      ),
+                      Container(
+                        width: 320,
+                        height: 440,
+                        child: Card(
+                          color: theme.colors.cardBackgroundColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: theme.sizing.borderRadius[0],
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(
+                              24,
                             ),
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                    child: LegendForm(
-                                      showSubmitButton: false,
-                                      buildSubmitButton: (key) {
-                                        return LegendButton(
-                                          text: LegendText(
-                                            text: "Anmelden",
-                                          ),
-                                          onPressed: () {
-                                            key.currentState?.validate();
-                                          },
-                                          margin: const EdgeInsets.symmetric(
-                                            vertical: 8,
-                                          ),
-                                          style: LegendButtonStyle.confirm(
-                                            height: 42,
-                                            color: theme.colors.primaryColor,
-                                            activeColor:
-                                                theme.colors.selectionColor,
-                                            textColor:
-                                                theme.colors.textColorLight,
-                                          ),
-                                        );
-                                      },
-                                      children: [
-                                        Expanded(child: Container()),
-                                        LegendFormField.text(
-                                          title: "Email",
-                                          isRequired: true,
-                                          text: LegendTextField(
-                                            decoration: LegendInputDecoration(
-                                              prefixIcon: const Icon(
-                                                Icons.email_outlined,
-                                              ),
-                                              textColor: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                        Expanded(child: Container()),
-                                        LegendFormField.text(
-                                          title: "Password",
-                                          isRequired: true,
-                                          text: LegendTextField(
-                                            decoration: LegendInputDecoration(
-                                              prefixIcon: const Icon(
-                                                Icons.password_outlined,
-                                              ),
-                                              textColor: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                        Row(
-                                          children: [
-                                            Expanded(child: Container()),
-                                            LegendButton(
+                            child: Column(
+                              children: [
+                                LegendText(
+                                  text: "Login",
+                                  textStyle: theme.typography.h5,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    children: [
+                                      Expanded(
+                                        child: LegendForm(
+                                          showSubmitButton: false,
+                                          buildSubmitButton: (key) {
+                                            return LegendButton(
                                               text: LegendText(
-                                                text: "Passwort vergessen?",
+                                                text: "Anmelden",
                                               ),
                                               onPressed: () {
-                                                print("password");
+                                                key.currentState?.validate();
                                               },
-                                              style: LegendButtonStyle.text(
-                                                activeColor:
-                                                    theme.colors.selectionColor,
-                                                color:
-                                                    theme.colors.textColorLight,
-                                                height: 34,
-                                                width: LegendUtils.calcTextSize(
-                                                      "Passwort vergessen?",
-                                                      theme.typography.h1,
-                                                    ).width +
-                                                    16,
-                                                textStyle: theme.typography.h1,
+                                              margin:
+                                                  const EdgeInsets.symmetric(
+                                                vertical: 12,
                                               ),
-                                            ),
-                                          ],
-                                        ),
-                                        Expanded(child: Container()),
-                                      ],
-                                    ),
-                                  ),
-                                  Center(
-                                    child: LegendButton(
-                                      text: RichText(
-                                        text: TextSpan(
-                                          text: 'oder ',
-                                          style: theme.typography.h1,
-                                          children: <TextSpan>[
-                                            TextSpan(
-                                              text: 'Registrieren',
-                                              style:
-                                                  theme.typography.h1.copyWith(
+                                              style: LegendButtonStyle.confirm(
+                                                height: 42,
                                                 color:
                                                     theme.colors.primaryColor,
-                                                fontWeight: FontWeight.bold,
+                                                activeColor:
+                                                    theme.colors.selectionColor,
+                                                textColor:
+                                                    theme.colors.textColorLight,
+                                              ),
+                                            );
+                                          },
+                                          children: [
+                                            Expanded(child: Container()),
+                                            LegendFormField.text(
+                                              title: "Email",
+                                              isRequired: true,
+                                              text: LegendTextField(
+                                                decoration:
+                                                    LegendInputDecoration(
+                                                  prefixIcon: const Icon(
+                                                    Icons.email_outlined,
+                                                  ),
+                                                  textColor: Colors.white,
+                                                ),
                                               ),
                                             ),
+                                            Expanded(child: Container()),
+                                            LegendFormField.text(
+                                              title: "Password",
+                                              isRequired: true,
+                                              text: LegendTextField(
+                                                decoration:
+                                                    LegendInputDecoration(
+                                                  prefixIcon: const Icon(
+                                                    Icons.password_outlined,
+                                                  ),
+                                                  textColor: Colors.white,
+                                                ),
+                                              ),
+                                            ),
+                                            Row(
+                                              children: [
+                                                Expanded(child: Container()),
+                                                LegendButton(
+                                                  text: LegendText(
+                                                    text: "Passwort vergessen?",
+                                                  ),
+                                                  onPressed: () {
+                                                    print("password");
+                                                  },
+                                                  style: LegendButtonStyle.text(
+                                                    activeColor: theme
+                                                        .colors.selectionColor,
+                                                    color: theme
+                                                        .colors.textColorLight,
+                                                    height: 34,
+                                                    width: LegendUtils
+                                                            .calcTextSize(
+                                                          "Passwort vergessen?",
+                                                          theme.typography.h1,
+                                                        ).width +
+                                                        16,
+                                                    textStyle:
+                                                        theme.typography.h1,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            Expanded(child: Container()),
                                           ],
                                         ),
                                       ),
-                                      onPressed: () {
-                                        print("password");
-                                      },
-                                      style: LegendButtonStyle.text(
-                                        activeColor:
-                                            theme.colors.selectionColor,
-                                        color: theme.colors.primaryColor,
-                                        height: 34,
-                                        width: LegendUtils.calcTextSize(
-                                              "Passwort vergessen?",
-                                              theme.typography.h1,
-                                            ).width +
-                                            16,
-                                        textStyle: theme.typography.h1,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
+                                      Center(
+                                        child: LegendButton(
+                                          text: RichText(
+                                            text: TextSpan(
+                                              text: 'oder ',
+                                              style: theme.typography.h1,
+                                              children: <TextSpan>[
+                                                TextSpan(
+                                                  text: 'Registrieren',
+                                                  style: theme.typography.h1
+                                                      .copyWith(
+                                                    color: theme
+                                                        .colors.primaryColor,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          onPressed: () {
+                                            print("password");
+                                          },
+                                          style: LegendButtonStyle.text(
+                                            activeColor:
+                                                theme.colors.selectionColor,
+                                            color: theme.colors.primaryColor,
+                                            height: 34,
+                                            width: LegendUtils.calcTextSize(
+                                                  "Passwort vergessen?",
+                                                  theme.typography.h1,
+                                                ).width +
+                                                16,
+                                            textStyle: theme.typography.h1,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
               ),
